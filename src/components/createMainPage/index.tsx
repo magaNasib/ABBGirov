4d392345e3e9b871e405cc873fe91209c0eeae1a
magaNasib <mnasibdev@gmail.com>
2023-12-22 10:32:07 +0400

    changed into httpClient

--- src/components/createMainPage/index.tsx
+++ src/components/createMainPage/index.tsx
@@ -8,6 +8,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useSWR from 'swr';
 import MyDatePicker from 'components/UI/MyDatePicker';
 import { MyInput } from 'components/UI/MyInput';
+import { httpClient } from 'httpClient';
 
 // interface IFormValues {
 //   customerId: number;
@@ -23,7 +24,13 @@ import { MyInput } from 'components/UI/MyInput';
 
 export interface IProps { }
 
+interface IProductData {
+  product: string;
+}
 
+interface ICustomerData {
+  fullname: string;
+}
 
 
 const CreateMain: React.FC<IProps> = () => {
@@ -35,18 +42,20 @@ const CreateMain: React.FC<IProps> = () => {
   const category = methods.watch('category');
   const customerId = methods.watch('customerId');
 
-  const apiUrl = `/customers/flex-customer-reader/v3/individual-customer-controller/getIndividualCustomerByCifUsingGET_1/${customerId}`
-    ;
-  const fetchProductData = async (url) => {
-    const response = await fetch(url);
-    return await response.json();
+  const apiUrl = `/customers/flex-customer-reader/v3/individual-customer-controller/getIndividualCustomerByCifUsingGET_1/${customerId}`;
+  const fetchProductData = async (url: string): Promise<IProductData> => {
+    const response: IProductData= await httpClient.get(url);
+    console.log(response);
+  
+    return response; // Return the entire Axios response object
   };
-  const fetchCustomerData = async (url) => {
+  
+
+  const fetchCustomerData = async (url:string): Promise<ICustomerData> => {
     if (customerId.toString().length !== 7) return
+    const response: ICustomerData = await httpClient.get(url);  
 
-
-    const response = await fetch(url);
-    return await response.json();
+    return response
   };
 
   const { data, error, isLoading: isCustomerIdLoading } = useSWR(customerId ? apiUrl : null, fetchCustomerData, {
@@ -57,15 +66,19 @@ const CreateMain: React.FC<IProps> = () => {
 
 
   const {
-    data: productData,
+    data: productData, // Assuming 'data' is the correct property name
     error: productDataError,
     isLoading: isCategoryLoading
-  } = useSWR(category ? `http://localhost:8082/products/product-code/${category}` : null, fetchProductData, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false
-  });
-  //   const [{ isCreateButttonExist }] = useAppContext();
+  } = useSWR(
+    category ? `http://localhost:8082/products/product-code/${category}` : null,
+    fetchProductData,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
+    }
+  );
+  
   const [{ setIsCreateButtonExist }] = useAppContext();
 
   const onSubmitHandler = methods.handleSubmit((data) => {
@@ -124,7 +137,7 @@ const CreateMain: React.FC<IProps> = () => {
                 render={({ field }) => (
                   <MyInput
                     {...field}
-                    value={data ? data.fullname : 'Müştəri'}
+                    value={data ? data?.fullname : 'Müştəri'}
                     placeholder="Ad Soyad Ata adı"
                     isLoading={isCustomerIdLoading}
                     disabled
@@ -177,7 +190,7 @@ const CreateMain: React.FC<IProps> = () => {
                     {...field}
                     disabled={true}
                     isLoading={isCategoryLoading}
-                    value={productData ? productData.product : 'Məhsul'}
+                    value={productData ? productData?.product : 'Məhsul'}
                     label="Məhsul"
                     ref={ref}
                   />
--- src/components/createOtherDetails/index.tsx
+++ src/components/createOtherDetails/index.tsx
@@ -1,52 +1,63 @@
 import { Box, FormControl, FormErrorMessage, FormLabel, Grid, GridItem, Heading, Select, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+// import { Box, Grid, GridItem, Heading,  SkeletonCircle, SkeletonText } from '@chakra-ui/react';
 import { IProps } from 'components/createMainPage';
-import DepositInfo from 'components/DepositInfo';
 import { MyInput } from 'components/UI/MyInput';
-// import DepositInfo from 'components/DepositInfo';
+import { httpClient } from 'httpClient';
+import DepositInfo from 'components/DepositInfo';
 import { Footer } from 'Layout/Footer';
 import { IFormValues } from 'pages/Create';
 import React, { useEffect } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { useNavigate, useParams } from 'react-router-dom';
-
-// interface IFormValues {
-//   propertyType: string;
-//   owner: string;
-//   propertyDetail: string;
-//   buildingCompany: string;
-//   city: string;
-//   district: string;
-//   municipality: string;
-//   'town/village': string;
-//   'construction-project': string;
-//   'land-designation': string;
-// }
 import useSWR from 'swr';
 
-// const fetchPledgesData = async (url) => {
-//   const response = httpClient.get(url);
-//   return await response
-// };
-const fetchPledgesData = async (url) => {
-  const response = await fetch(url);
-  return await response.json();
+
+interface IPladge {
+  colletralCode: string
+  customerCIF: string
+  fullname: string
+  customerPledge: {
+    describe: string
+    endDate: string
+    pledgedCurrency: string
+    pledgesValue: number
+    startDate: string
+  }
+  data: {
+    deposit: {
+      branch: string
+      currency: string
+      reference: string
+    }[]
+    productCode: string
+    questions: {
+      key: keyof IFormValues
+      value: string
+      type: string
+      maxLenght: number
+      minLenght: number
+      items: {
+        key: string
+        value: string
+      }[]
+    }[]
+  }[]
+}
+
+const fetchPledgesData = async (url: string): Promise<IPladge> => {
+  const response: IPladge = await httpClient.get(url);
+  return response;
 };
 const postData = async (url = "", data = {}) => {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
+  const response = await httpClient.post(url, data);
+  return response
 }
 
 const OtherInformation: React.FC<IProps> = () => {
   const methods = useFormContext<IFormValues>();
   const navigate = useNavigate()
   const { colletralCode } = useParams();
-  const { data: pledgeData,isLoading } = useSWR(`http://localhost:8082/pledges/${colletralCode}`, fetchPledgesData, {
+  const { data: pledgeData, isLoading } = useSWR(`http://localhost:8082/pledges/${colletralCode}`, fetchPledgesData, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false
@@ -54,80 +65,79 @@ const OtherInformation: React.FC<IProps> = () => {
 
   const onSubmitHandler = methods.handleSubmit((data) => {
 
+
     postData("/pledges", data).then((data) => {
       console.log(data);
     });
     navigate('/abb-mf-pledge/success')
-    navigate('/abb-mf-pledge/success')
   });
-
-
 
   useEffect(() => {
     !methods.getValues('customerId') && navigate('/abb-mf-pledge/create')
   }, [])
+
   return (
     <>
       {pledgeData?.data[0].deposit.length ? <DepositInfo /> : ''}
-      {isLoading?
-       <Box padding='6' boxShadow='lg' bg='white' width="100%">
-       <SkeletonCircle size='10' />
-       <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-     </Box>:
+      {isLoading ?
+        <Box padding='6' boxShadow='lg' bg='white' width="100%">
+          <SkeletonCircle size='10' />
+          <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+        </Box> :
         <Box padding="24px" w={'100%'} bg="white" borderRadius="12px" margin="0 auto">
-        <Grid templateColumns="repeat(3, 1fr)" gap="24px">
-          <GridItem colSpan={3}>
-            <Heading as="h3" size="lg">
-              Digər məlumatlar
-            </Heading>
-          </GridItem>
+          <Grid templateColumns="repeat(3, 1fr)" gap="24px">
+            <GridItem colSpan={3}>
+              <Heading as="h3" size="lg">
+                Digər məlumatlar
+              </Heading>
+            </GridItem>
 
-          {pledgeData?.data[0].questions.map((question, index) => {
-            return question.type === 'select' ? (
-              <GridItem key={index} colSpan={1}>
-                <FormControl isInvalid={!!methods.formState.errors[question.key]}>
-                  <Controller
-                    control={methods.control}
-                    rules={{
-                      required: 'This field is required'
-                    }}
-                    name={question.key}
-                    render={({ field }) => (
-                      <>
-                        <FormLabel>{question.value}</FormLabel>
-                        <Select placeholder="Seçin" value={field.value} onChange={field.onChange} {...field}>
-                          {question.items.map(({ key, value }) => (
-                            <option key={key}>{value}</option>
-                          ))}
-                        </Select>
-                      </>
-                    )}
-                  />
-                  <FormErrorMessage>
-                    {methods.formState.errors[question.key]?.message}
-                  </FormErrorMessage>
-                </FormControl>
-              </GridItem>
-            ) : (
-              <GridItem key={index} colSpan={1}>
-                <FormControl isInvalid={!!methods.formState.errors[question.key]}>
-                  <Controller
-                    control={methods.control}
-                    rules={{
-                      required: 'This field is required'
-                    }}
-                    name={question.key}
-                    render={({ field }) => <MyInput {...field} placeholder="Daxil edin" label={question.value} />}
-                  />
-                  <FormErrorMessage color={'red'} fontSize={'14px'}>
-                    {methods.formState.errors[question.key]?.message}
-                  </FormErrorMessage>
-                </FormControl>
-              </GridItem>
-            );
-          })}
-        </Grid>
-      </Box>  
+            {pledgeData?.data[0].questions.map((question, index) => {
+              return question.type === 'select' ? (
+                <GridItem key={index} colSpan={1}>
+                  <FormControl isInvalid={!!methods.formState.errors[question.key]}>
+                    <Controller
+                      control={methods.control}
+                      rules={{
+                        required: 'This field is required'
+                      }}
+                      name={question.key}
+                      render={({ field }) => (
+                        <>
+                          <FormLabel>{question.value}</FormLabel>
+                          <Select placeholder="Seçin" value={field.value} onChange={field.onChange} {...field}>
+                            {question.items.map(({ key, value }) => (
+                              <option key={key}>{value}</option>
+                            ))}
+                          </Select>
+                        </>
+                      )}
+                    />
+                    <FormErrorMessage>
+                      {methods.formState.errors[question.key]?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </GridItem>
+              ) : (
+                <GridItem key={index} colSpan={1}>
+                  <FormControl isInvalid={!!methods.formState.errors[question.key]}>
+                    <Controller
+                      control={methods.control}
+                      rules={{
+                        required: 'This field is required'
+                      }}
+                      name={question.key}
+                      render={({ field }) => <MyInput {...field} placeholder="Daxil edin" label={question.value} />}
+                    />
+                    <FormErrorMessage color={'red'} fontSize={'14px'}>
+                      {methods.formState.errors[question.key]?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </GridItem>
+              );
+            })}
+          </Grid>
+        </Box>
       }
       <Footer onSubmitHandler={onSubmitHandler} isCreateMode={!!colletralCode} />
     </>
