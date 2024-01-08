3699111c38bb10d032812dc51b0e7e941201d401
RaisaYusifli <yusiflireise2003@gmail.com>
2024-01-08 20:43:23 +0400

    some errors fixed

--- src/components/UI/MyDatePicker.tsx
+++ src/components/UI/MyDatePicker.tsx
@@ -27,7 +27,10 @@ const ChakraDatePicker = chakra(DatePicker, {
       border: 'none',
     },
     border: '1px solid #d3d3d3',
-    borderRadius: '5px'
+    borderRadius: '5px',
+    position:'relative',
+    zIndex:9999999
+    
   },
 });
 const MyDatePicker = ({ label, field, disabled = false }: IProps) => {
