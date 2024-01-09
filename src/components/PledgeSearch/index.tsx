c884654966eb61acf09171f18de24e83d6ad559c
magaNasib <mnasibdev@gmail.com>
2024-01-09 08:48:42 +0400

    deleted some directories

--- src/components/PledgeSearch/index.tsx
+++ src/components/PledgeSearch/index.tsx
@@ -104,7 +104,7 @@ export default function EditSelection() {
       <Box w={"100%"} mt="20px" mb="20px" className="bottomBar" bg={'white'} borderRadius="12px" >
         <Grid templateColumns="2fr 2fr" alignItems={'center'} display="flex" padding={'1rem 2rem'} justifyContent="space-between" >
           <GridItem>
-            <Button padding={'.5rem 1rem'} color={"#fff"} bg={"red"}>Ləvğ et</Button>
+            <Button padding={'.5rem 1rem'} color={"#fff"} bg={"red"} onClick={()=>navigate('/abb-mf-pledge')}>Ləvğ et</Button>
           </GridItem>
           <GridItem display="flex" gap="16px" alignItems="center" justifyContent="end" >
             <Button padding={'.5rem 1rem'} color={"#fff"} bg={"blue"} type='submit' onClick={onSubmit}>Axtar</Button>
--- src/components/UI/MyDatePicker.tsx
+++ src/components/UI/MyDatePicker.tsx
@@ -30,7 +30,7 @@ const ChakraDatePicker = chakra(DatePicker, {
     border: '1px solid #d3d3d3',
     borderRadius: '5px',
     position:'relative',
-    zIndex:9999999
+    zIndex:52
     
   },
 });
